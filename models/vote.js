0b5ce4c717356844511b55ab5642fc3501db30ed
CaptMerica <rmerica@live.com>
2023-02-28 16:25:24 -0500

    update profiles controller

--- models/vote.js
+++ models/vote.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Vote extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Vote.belongsTo(models.Profile, { foreignKey: 'voterId' })
       Vote.belongsTo(models.Profile, { foreignKey: 'profileId' })
