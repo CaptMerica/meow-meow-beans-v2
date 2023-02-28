40f397b97ab42c982c4efb03c2eede5b10d083ab
CaptMerica <rmerica@live.com>
2023-02-28 12:16:47 -0500

    create castVote function

--- models/vote.js
+++ models/vote.js
@@ -10,13 +10,39 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Vote.belongsTo(models.Profile, { foreignKey: 'voterId' })
+      Vote.belongsTo(models.Profile, { foreignKey: 'profileId' })
     }
   }
   Vote.init({
-    value: DataTypes.INTEGER,
-    profileId: DataTypes.INTEGER,
-    voterId: DataTypes.INTEGER
+    value: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 0,
+				max: 5,
+      },
+    },
+
+    profileId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      },
+    },
+
+    voterId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      },
+    },
+
   }, {
     sequelize,
     modelName: 'Vote',
